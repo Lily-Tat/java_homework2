c205d18ca70a564d597206899a1ec27f71e770e7
Lilia Tatarintseva <juicy-1_3@inbox.ru>
2021-10-13 11:40:34 +0400

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 2500;
+        int moneybonuse = 20;
+        int miles = ticketprice / moneybonuse;
+        System.out.println(miles);
+    }
+}
